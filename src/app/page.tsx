dc06f8ad8d5d3264fd2e3708abe8443855e133a2
Joshua Evuetapha <evuetaphajoshua@gmail.com>
2025-02-22 16:51:46 +0000

    landing page slide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,23 @@
 import Image from "next/image";
 import SocialLink from "./components/social-link";
 import AuthBtn from "./components/authBtn";
+import LandingCarousel from "./components/landingCarousel";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center bg-[#050505] h-screen py-16">
-      <div className="flex justify-center w-full mb-10">
+    <main className="bg-[#050505] h-screen overflow-clip">
+
+      <div className="absolute z-10 md:top-[6%] flex justify-center w-full mb-10">
         <Image src={"/logo.png"} alt="logo" width={140} height={36} />
       </div>
 
-      <div className="relative flex flex-col justify-center items-center">
-        {/* <LandingSlider /> */}
+      <div className="absolute w-full overflow-clip md:h-[80vh] z-10">
+        <LandingCarousel />
+      </div>
 
-        <div className="flex h-[40vh] z-10 gap-5">
-          <Image
-            src={"/images/landing-1.png"}
-            height={405}
-            width={405}
-            alt="view"
-          />
-          <Image
-            src={"/images/landing-2.jpeg"}
-            height={405}
-            width={405}
-            alt="view"
-          />
-          <Image
-            src={"/images/landing-3.png"}
-            height={405}
-            width={405}
-            alt="view"
-          />
-        </div>
+      <div className="absolute bottom-[15vh] md:bottom-[20vh] flex justify-center w-full ">
 
-        <div className="absolute top-[360px] flex flex-col justify-center items-center border-primary-10/50 border-[1px] w-[350px] rounded-xl gap-4 py-12">
+        <div className="flex z-20 flex-col justify-center items-center border-primary-10/50 border-x-[1px] border-b-[1px] w-[350px] rounded-xl gap-4 py-12">
           <AuthBtn icon={<div></div>} provider="google">
             Continue with Google
           </AuthBtn>
@@ -42,26 +26,19 @@ export default function Home() {
           <AuthBtn icon={<div></div>} provider="apple">
             Continue with Apple
           </AuthBtn>
+        </div> 
 
-          {/* <AuthBtn
-            icon={
-              <div>
+      </div>
 
-              </div>
-            }>
-            Continue with Wallet
-          </AuthBtn> */}
+      <div className="flex justify-center w-full absolute bottom-10"> 
+        <div className="flex gap-9 justify-center py-3 px-6 borderColor-primary-10/50 border-[1px] rounded-[20px]">
+          <SocialLink icon="/icons/discord.png" url="" />
+          <SocialLink icon="/icons/linkedin.png" url="" />
+          <SocialLink icon="/icons/telegram.png" url="" />
+          <SocialLink icon="/icons/x.png" url="" />
         </div>
       </div>
 
-      <div className="flex-grow"></div>
-
-      <div className="flex gap-9 justify-center py-3 px-6 borderColor-primary-10/50 border-[1px] rounded-[20px]">
-        <SocialLink icon="/icons/discord.png" url="" />
-        <SocialLink icon="/icons/linkedin.png" url="" />
-        <SocialLink icon="/icons/telegram.png" url="" />
-        <SocialLink icon="/icons/x.png" url="" />
-      </div>
     </main>
   );
 }
