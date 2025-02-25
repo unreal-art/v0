6ace721e56532fc2771e932a90441e653ef0ddd0
codypharm <williamikeji@gmail.com>
2025-02-25 13:43:18 +0100

    feat: zustand and generation store watching

---
+++ src/stores/generationStore.ts
@@ -0,0 +1,34 @@
+// src/stores/counter-store.ts
+import { createStore } from "zustand/vanilla";
+
+export type GenerationState = {
+  isActive: boolean;
+};
+
+export type GenerationActions = {
+  startGeneration: () => void;
+  stopGeneration: () => void;
+};
+
+export type GenerationStore = GenerationState & GenerationActions;
+
+//initialize
+export const initGenerationStore = (): GenerationState => {
+  return { isActive: false };
+};
+
+export const defaultInitState: GenerationState = {
+  isActive: false,
+};
+
+export const createGenerationStore = (
+  initState: GenerationState = defaultInitState,
+) => {
+  return createStore<GenerationStore>()((set) => ({
+    ...initState,
+    // startGeneration: () => set((state) => ({ count: state.count - 1 })),
+    // incrementCount: () => set((state) => ({ count: state.count + 1 })),
+    startGeneration: () => set(() => ({ isActive: true })),
+    stopGeneration: () => set(() => ({ isActive: false })),
+  }));
+};
