4666cf69dd612d4009a699679c3af0eaaa3be667
codypharm <williamikeji@gmail.com>
2025-03-06 23:37:45 +0100

    feat: reply submission

--- src/app/home/components/comments/comment.tsx
+++ src/app/home/components/comments/comment.tsx
@@ -5,47 +5,70 @@ import useAuthorUsername from "@/hooks/useAuthorUserName";
 import Image from "next/image";
 import { useLikeComment, useUnlikeComment } from "@/hooks/useComments";
 
-export default function Comment({data, setReplyTo} : {data: CommentWithUser, setReplyTo: (id: string | null) => void}) {
+export default function Comment({
+  data,
+  setReplyTo,
+  // setCommentToReply,
+}: {
+  data: CommentWithUser;
+  setReplyTo: (data: CommentWithUser | null) => void;
+  // setCommentToReply: (data: CommentWithUser) => void;
+}) {
   const { data: user } = useAuthorUsername(data.user_id);
   const likeComment = useLikeComment(data.post_id.toString());
   const unlikeComment = useUnlikeComment(data.post_id.toString());
 
   return (
-    <div className="flex gap-2 py-2">
-      <div className="h-12 w-12">
-        <Image
-          src={data.avatar_url || "/profile.jpg"}
-          width={48}
-          height={48}
-          alt="profile"
-          className="rounded-full"
-        />
-      </div>
-      <div className="flex gap-2 w-full">
-        <div className="flex flex-col basis-1/6">
-          <p className="text-primary-4 text-md font-medium">{user}</p>
-          <p className="text-xs">{timeAgo(data.created_at)}</p>
+    <div className=" gap-2 py-2 border-b border-primary-10">
+      <div className="flex w-full  gap-2">
+        <div className="h-12 w-12">
+          <Image
+            src={data.avatar_url || "/profile.jpg"}
+            width={48}
+            height={48}
+            alt="profile"
+            className="rounded-full"
+          />
         </div>
-        <div className="flex-grow">
-          <p className="text-primary-6 text-sm flex-grow">{data.content}</p>
-          <button className="text-primary-4" onClick={() => setReplyTo(data.id)}>reply</button>
-        </div>
-        <div className="justify-end">
-          <button
-            className="flex gap-1 items-center"
-            onClick={() =>
-              data.user_liked
-                ? unlikeComment.mutate(data.id)
-                : likeComment.mutate(data.id)
-            }
-          >
-            {data.user_liked ? (
-              <HeartFillIcon color="#FFFFFF" />
-            ) : (
-              <HeartIcon color="#FFFFFF" />
-            )}
-          </button>
-          <p className="text-center">{data.like_count}</p>
+        <div className="w-full">
+          <div className="flex flex-col basis-1/6">
+            <p className="text-primary-4 text-md font-medium whitespace-nowrap">
+              {user}
+            </p>
+            <p className="text-xs">{timeAgo(data.created_at)}</p>
+          </div>
+
+          <div className="flex gap-2 w-full pt-2">
+            <div className="flex-grow space-y-4 gap-4">
+              <p className="text-primary-6 text-sm flex-grow">{data.content}</p>
+              <button
+                className="text-primary-4 "
+                onClick={() => {
+                  data.username = user as string;
+                  setReplyTo(data);
+                }}
+              >
+                reply
+              </button>
+            </div>
+            <div className="justify-end">
+              <button
+                className="flex gap-1 items-center"
+                onClick={() =>
+                  data.user_liked
+                    ? unlikeComment.mutate(data.id)
+                    : likeComment.mutate(data.id)
+                }
+              >
+                {data.user_liked ? (
+                  <HeartFillIcon color="#FFFFFF" />
+                ) : (
+                  <HeartIcon color="#FFFFFF" />
+                )}
+              </button>
+              <p className="text-center">{data.like_count}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/app/home/components/followingBtn.tsx
+++ src/app/home/components/followingBtn.tsx
@@ -2,39 +2,36 @@ import { useDoesUserFollow } from "@/hooks/useDoesUserFollow";
 import { useUser } from "@/hooks/useUser";
 import { useToggleFollow } from "@/hooks/useToggleFollow";
 
+export function Following({ authorId }: { authorId: string }) {
+  const { userId } = useUser();
+  const { data: isFollowing, isLoading } = useDoesUserFollow(
+    userId as string,
+    authorId,
+  );
+  const toggleFollowMutation = useToggleFollow();
 
+  const handleFollowToggle = () => {
+    toggleFollowMutation.mutate({
+      followerId: userId as string,
+      followeeId: authorId,
+    });
+  };
 
-export function Following({authorId}: { authorId: string }) {
-
-    const { userId } = useUser();
-    const { data: isFollowing, isLoading } = useDoesUserFollow(
-        userId as string,
-        authorId,
-    );
-    const toggleFollowMutation = useToggleFollow();
-
-    const handleFollowToggle = () => {
-        toggleFollowMutation.mutate({
-        followerId: userId as string,
-        followeeId: authorId
-        });
-    };
-
-    return (
-        <div className="bg-primary-11 rounded-t-3xl my-3">
-
-            {
-                userId !== authorId && (
-                    <button
-                        disabled={toggleFollowMutation.isPending}
-                        onClick={handleFollowToggle}
-                        className={`flex items-center justify-center gap-1 rounded-full h-8 w-24 px-2 py-1 border-[1px] border-primary-8 bg-[#DADADA] text-primary-11 ${isFollowing ? "bg-transparent" : "bg-primary-10"}`}>
-                        <p className="text-primary-5 text-sm">
-                        {isLoading ? "Loading..." : isFollowing ? "Unfollow" : "Follow"}
-                        </p>
-                    </button>
-            )}
-
-        </div>
-    );
+  return (
+    <div className=" rounded-t-3xl my-3">
+      {userId !== authorId && (
+        <button
+          disabled={toggleFollowMutation.isPending}
+          onClick={handleFollowToggle}
+          className={`flex items-center justify-center gap-1 rounded-full h-8 w-24 px-2 py-1 border-[1px] border-primary-8 bg-[#DADADA] text-primary-11 ${isFollowing ? "bg-transparent" : "bg-primary-10"}`}
+        >
+          {!isLoading && (
+            <p className="text-primary-5 text-sm">
+              {isFollowing ? "Unfollow" : "Follow"}
+            </p>
+          )}
+        </button>
+      )}
+    </div>
+  );
 }
