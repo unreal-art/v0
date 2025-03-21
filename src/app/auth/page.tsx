c6d423d066186ec8192c13b76bda2943300e8325
codypharm <williamikeji@gmail.com>
2025-03-21 21:00:14 +0100

    border-remved

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -25,7 +25,7 @@ export default async function Auth() {
       <div
         style={{ zIndex: 1000 }}
         className=" absolute bottom-28 sm:bottom-32 md:bottom-28  inset-x-0 mx-auto flex flex-col justify-center items-center z-10
-                   border-primary-10 border-x-[1px] border-b-[1px]
+
                    w-[350px] max-w-[90%] sm:max-w-[80%]
                    rounded-b-xl gap-4 py-12  shadow-lg"
       >
