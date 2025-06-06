f9fe8a7886333c1c5f9101d22dfa1762a21aa31a
codypharm <williamikeji@gmail.com>
2025-06-06 18:05:14 +0100

    fix/ jerky screen

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -6,10 +6,8 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
-  const encodedRedirectTo = requestUrl.searchParams.get("redirect") || "/home";
-  // const torusUser = decodeURIComponent(
-  //   requestUrl.searchParams.get("torus_user") || "",
-  // );
+  const encodedRedirectTo = requestUrl.searchParams.get("redirect") || "/";
+
   const redirectTo = decodeURIComponent(encodedRedirectTo);
 
   const supabase = await createClient();
